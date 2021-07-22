8ff3ebae81b0e6f7f3b60aacce81d6af339e26bd
Artem Kokos <artem-kokos@mail.ru>
2021-07-22 21:45:56 +0700

    Remove redundant close() call for listen socket
    Because flag SOCK_CLOEXEC used

--- remote_shell.c
+++ remote_shell.c
@@ -95,28 +95,18 @@ int main(int argc, char** argv) {
 
         pid_t pid = fork();
         if (0 == pid) {
-            /* Close listening socket in child just in case.
-             * https://stackoverflow.com/a/32403609
-             */
-            close(server_socket_fd);
-
             dup2(client_socket_fd, STDIN_FILENO);
             dup2(client_socket_fd, STDOUT_FILENO);
             dup2(client_socket_fd, STDERR_FILENO);
 
             execl(cmd_shell, cmd_shell, NULL);
         } else if (-1 == pid) {
-            close(client_socket_fd);
             fprintf(stderr, "WARNING: Can't fork to new terminal (%s)\n", strerror(errno));
-            continue;
         } else {
-            close(client_socket_fd);
             fprintf(stdout, "New connection from %s, process PID: %d\n", inet_ntoa(client_addr.sin_addr), pid);
         }
-
-        /* TODO: waitpid() */
-        /* TODO: Close all on exit and handle signals */
     }
+    /* TODO: Close all on exit */
 
     close(server_socket_fd);
     return 0;
